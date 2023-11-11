e7d50d570a9feb921692913250259dfd92b3b465
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-11-11 18:41:04 +0800

    Vite

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import './style.css'
 import App from '@/App.vue'
 import router from '@/Router'
 
+// async js chunk
+// import('./bootstrap.ts').then(mod => {
+//     console.log(mod);
+// })
+
+// console.log("hit");
+
+
 const pinia = createPinia()
 
 // option API
